075d9fc19a610b2f6e33d332daea14723fcdc5b9
Francisco.eck <francisco.eckhardt@polytechnique.org>
2017-04-10 12:32:51 -0300

    Updates .gitignore, corrects feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,15 +1,15 @@
 import pandas as pd
-
+from sklearn.base import TransformerMixin
 
 class FeatureExtractor(TransformerMixin):
 
     def __init__(self):
-        pass
+            pass
 
     def fit(self, X_df, y):
         return self
 
-	def transform(self, X_df):
-		X_df['yyyymm'] -= X_df['yyyymm'][0]
-		X_df['yyyymm'] /= X_df['yyyymm'].iget(-1)
-	    return X_df.values
+    def transform(self, X_df):
+        X_df['yyyymm'] -= X_df['yyyymm'][0]
+        X_df['yyyymm'] /= X_df['yyyymm'].iget(-1)
+        return X_df.values
